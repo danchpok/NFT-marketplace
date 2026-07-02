5d3c7b9e4ed1a5cc2e9f61590987642184d250cd
daryn <darynseri511@gmail.com>
2026-07-02 16:00:24 +0500

    feat: поработал над странией регистрации ну положил начало но пока ссс не работает хз почему

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './components/auth/login/login'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Login], // 2. Добавляем LoginComponent сюда через запятую
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
